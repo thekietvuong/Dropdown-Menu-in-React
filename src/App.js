92233ebf5ae6b84499f06524087495ec83a98630
Kiet Vuong <vuongthekiet1@gmail.com>
2022-08-21 12:13:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import logo from './logo.svg';
+import user from '../src/img/user.png';
+import edit from '../src/img/edit.png';
+import inbox from '../src/img/envelope.png';
+import settings from '../src/img/settings.png';
+import help from '../src/img/question.png';
+import logout from '../src/img/log-out.png';
 import './App.css';
 
+import React, {useState, useEffect, useRef} from 'react';
+
 function App() {
+
+  const [open, setOpen] = useState(false);
+
+  let menuRef = useRef();
+
+  useEffect(() => {
+    let handler = (e)=>{
+      if(!menuRef.current.contains(e.target)){
+        setOpen(false);
+        console.log(menuRef.current);
+      }      
+    };
+
+    document.addEventListener("mousedown", handler);
+    
+
+    return() =>{
+      document.removeEventListener("mousedown", handler);
+    }
+
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='menu-container' ref={menuRef}>
+        <div className='menu-trigger' onClick={()=>{setOpen(!open)}}>
+          <img src={user}></img>
+        </div>
+
+        <div className={`dropdown-menu ${open? 'active' : 'inactive'}`} >
+          <h3>The Kiet<br/><span>Website Designer</span></h3>
+          <ul>
+            <DropdownItem img = {user} text = {"My Profile"}/>
+            <DropdownItem img = {edit} text = {"Edit Profile"}/>
+            <DropdownItem img = {inbox} text = {"Inbox"}/>
+            <DropdownItem img = {settings} text = {"Settings"}/>
+            <DropdownItem img = {help} text = {"Helps"}/>
+            <DropdownItem img = {logout} text = {"Logout"}/>
+          </ul>
+        </div>
+      </div>
     </div>
+  );
+}
+
+function DropdownItem(props){
+  return(
+    <li className = 'dropdownItem'>
+      <img src={props.img}></img>
+      <a> {props.text} </a>
+    </li>
   );
 }
 
